f4a25cd6214f8cb2fba1c91546c78576f32909fe
Abir <talukdar2107049@stud.kuet.ac.bd>
2025-08-21 20:18:48 +0600

    Grade class

---
+++ src/main/java/com/coveros/demo/helloworld/Grade.java
@@ -0,0 +1,17 @@
+package com.coveros.demo.helloworld;
+public class Grade {
+
+    public String getGrade(int marks) {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else if (marks >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
